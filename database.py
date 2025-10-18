bf51ce13350d43d625478ff38fdcb05bac3d56a2
Jetross <jetrossneri07@gmail.com>
2025-10-18 10:19:57 +0800

    first commit

---
+++ database.py
@@ -0,0 +1,15 @@
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy import Column, Integer, String
+
+DATABASE_URL = "postgresql+asyncpg://postgres:password@localhost:5432/postgres"
+
+engine = create_async_engine(DATABASE_URL, echo=True)
+SessionLocal = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+Base = declarative_base()
+
+class ItemDB(Base):
+    __tablename__ = "items"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    description = Column(String, nullable=True)
